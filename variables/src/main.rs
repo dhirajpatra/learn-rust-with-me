3b0dd21556badc47edd9b956d2ae5c535b702a47
Dhiraj Patra <dhiraj.patra@gmail.com>
2025-12-25 06:46:18 +0530

    udpated

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -23,4 +23,47 @@ fn main() {
     // println!("Space before trimming: '{}'", space);
     // space = space.len(); // This line will cause a compile-time error because space is a &str
     // println!("Length of space after shadowing: {}", space);
+
+    let x = 2.0;
+    let y: f32 = 3.0;
+
+    // addition
+    let sum = x + y;
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3; // Results in -1
+
+    // remainder
+    let remainder = 43 % 5;
+
+    println!(
+        "Sum: {}, Difference: {}, Product: {}, Quotient: {}, Truncated: {}, Remainder: {}",
+        sum, difference, product, quotient, truncated, remainder
+    );
+
+    let t = true;
+    let f: bool = false;
+    println!("Boolean values: t = {}, f = {}", t, f);
+
+    let c = 'z';
+    let z: char = 'z';
+    let heart_eyed_cat = '😻';
+    println!("Characters: {}, {}, {}", c, z, heart_eyed_cat);
+    println!("c == z: {}", c == z);
+
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (_a, _b, _c) = tup;
+    println!("The value of _a, _b, _c are: {}, {}, {}", _a, _b, _c);
+
+    let tup2 = (500, 6.4, 1);
+
+    let (x, y, z) = tup2;
+    println!("The value of x, y, z are: {}, {}, {}", x, y, z);
 }
