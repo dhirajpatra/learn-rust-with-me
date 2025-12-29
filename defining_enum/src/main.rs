7bea17bc933a52d81e851767d694a1d27b38e633
Dhiraj Patra <dhiraj.patra@gmail.com>
2025-12-29 09:22:39 +0530

    updated

---
+++ defining_enum/src/main.rs
@@ -0,0 +1,133 @@
+fn main() {
+    let my_color = Color::Green;
+
+    match my_color {
+        Color::Red => println!("The color is Red"),
+        Color::Green => println!("The color is Green"),
+        Color::Blue => println!("The color is Blue"),
+    }
+
+    let ipv_four = IpAddrKind::V4;
+    let ipv_six = IpAddrKind::V6;
+    route_kind(ipv_four);
+    route_kind(ipv_six);
+
+    // using struct with enum
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    route_struct(home);
+    route_struct(loopback);
+
+    // using enum with associated data
+    let home = IpAddress::V4(String::from("127.0.0.1"));
+    let loopback = IpAddress::V6(String::from("::1"));
+
+    route(home);
+    route(loopback);
+
+    // using enum with multiple associated data types
+    let home = IpAddressWithParts::V4(127, 0, 0, 1);
+    let loopback = IpAddressWithParts::V6(String::from("::1"));
+
+    // matching enum with multiple associated data types
+    let message1 = Message::Quit;
+    let message2 = Message::Move { x: 10, y: 20 };
+    let message3 = Message::Write(String::from("Hello, World!"));
+    let message4 = Message::ChangeColor(255, 0, 0);
+    message1.call();
+    message2.call();
+    message3.call();
+    message4.call();
+}
+
+// custom datatype with enum
+enum Color {
+    Red,
+    Green,
+    Blue,
+}
+
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+fn route_struct(ip: IpAddr) {
+    match ip.kind {
+        IpAddrKind::V4 => println!("This is an IPv4 address: {}", ip.address),
+        IpAddrKind::V6 => println!("This is an IPv6 address: {}", ip.address),
+    }
+}
+
+fn route_kind(ip_kind: IpAddrKind) {
+    match ip_kind {
+        IpAddrKind::V4 => println!("This is an IPv4 address"),
+        IpAddrKind::V6 => println!("This is an IPv6 address"),
+    }
+}
+
+// another way to define enum with associated data
+// so no need for separate struct
+enum IpAddress {
+    //  The name of each enum variant that we define also becomes a function
+    // that constructs an instance of the enum
+    V4(String),
+    V6(String),
+}
+
+fn route(ip: IpAddress) {
+    match ip {
+        IpAddress::V4(address) => println!("This is an IPv4 address: {}", address),
+        IpAddress::V6(address) => println!("This is an IPv6 address: {}", address),
+    }
+}
+
+// enum with multiple associated data types
+enum IpAddressWithParts {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+// this enum holding four variants in different types
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+// all below structs holding the same data as above enum
+struct QuitMessage; // unit struct
+struct MoveMessage {
+    x: i32,
+    y: i32,
+}
+struct WriteMessage(String); // tuple struct
+struct ChangeColorMessage(i32, i32, i32); // tuple struct
+
+// impl block for enum Message from structs
+impl Message {
+    fn call(&self) {
+        match self {
+            Message::Quit => println!("Quit message"),
+            Message::Move { x, y } => println!("Move message to x: {}, y: {}", x, y),
+            Message::Write(text) => println!("Write message: {}", text),
+            Message::ChangeColor(r, g, b) => {
+                println!("Change color to red: {}, green: {}, blue: {}", r, g, b)
+            }
+        }
+    }
+}
